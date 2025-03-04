dacdf12383daf309e0c3f6cfa51ff9684da96660
Viswadarshan R R <115559150+viswadarshan-024@users.noreply.github.com>
2025-03-04 22:55:31 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+if (!localStorage.getItem("vite-ui-theme")) {
+  document.documentElement.classList.add("dark");
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
