9c15b83983d57adfda52d71633d32de92e0f12d7
Viswadarshan R R <115559150+viswadarshan-024@users.noreply.github.com>
2025-03-04 22:45:26 +0530

    Update ThemeProvider.tsx

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,7 +1,7 @@
 
 import { createContext, useContext, useEffect, useState } from "react";
 
-type Theme = "dark" | "light" | "system;
+type Theme = "dark" | "light" | "system";
 // type Theme = "dark";
 
 
